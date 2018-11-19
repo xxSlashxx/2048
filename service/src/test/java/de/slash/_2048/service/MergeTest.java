4c2e46d981162b2dd867f64e917040cb9476ae7b
xxSlashxx <roland.speckmeier@gmx.de>
2018-11-19 17:42:50 +0100

    Added additional tests

--- service/src/test/java/de/slash/_2048/service/MergeTest.java
+++ service/src/test/java/de/slash/_2048/service/MergeTest.java
@@ -65,6 +65,46 @@ class MergeTest
         testMerge(input, Direction.RIGHT, expected);
     }
 
+    @Test
+    void testSample8()
+    {
+        Cell[] input = {new Cell(1, 0, 2), new Cell(1, 1, 2), new Cell(1, 2, 2), new Cell(1, 3, null)};
+        Cell[] expected = {new Cell(1, 0, 4), new Cell(1, 1, 2), new Cell(1, 2, null), new Cell(1, 3, null)};
+        testMerge(input, Direction.LEFT, expected);
+    }
+
+    @Test
+    void testSample9()
+    {
+        Cell[] input = {new Cell(1, 0, null), new Cell(1, 1, 2), new Cell(1, 2, 2), new Cell(1, 3, 2)};
+        Cell[] expected = {new Cell(1, 0, null), new Cell(1, 1, null), new Cell(1, 2, 2), new Cell(1, 3, 4)};
+        testMerge(input, Direction.RIGHT, expected);
+    }
+
+    @Test
+    void testSample10()
+    {
+        Cell[] input = {new Cell(1, 0, null), new Cell(1, 1, 2), new Cell(1, 2, 2), new Cell(1, 3, 2)};
+        Cell[] expected = {new Cell(1, 0, null), new Cell(1, 1, null), new Cell(1, 2, 2), new Cell(1, 3, 4)};
+        testMerge(input, Direction.DOWN, expected);
+    }
+
+    @Test
+    void testSample11()
+    {
+        Cell[] input = {new Cell(1, 0, 2), new Cell(1, 1, 2), new Cell(1, 2, 2), new Cell(1, 3, null)};
+        Cell[] expected = {new Cell(1, 0, 4), new Cell(1, 1, 2), new Cell(1, 2, null), new Cell(1, 3, null)};
+        testMerge(input, Direction.UP, expected);
+    }
+
+    @Test
+    void testSample12()
+    {
+        Cell[] input = {new Cell(1, 0, 2), new Cell(1, 1, null), new Cell(1, 2, 2), new Cell(1, 3, 8)};
+        Cell[] expected = {new Cell(1, 0, null), new Cell(1, 1, null), new Cell(1, 2, 4), new Cell(1, 3, 8)};
+        testMerge(input, Direction.DOWN, expected);
+    }
+
     private void testMerge(Cell[] input, Direction direction, Cell[] expected)
     {
         GameBoardService service = new GameBoardService();

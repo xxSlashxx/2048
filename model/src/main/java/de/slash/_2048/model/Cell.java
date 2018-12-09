6a5e5af8e1915f7ef1d11d49cb0db680137b51e7
xxSlashxx <roland.speckmeier@gmx.de>
2018-12-09 13:29:23 +0100

    Removed unnecessary setters

--- model/src/main/java/de/slash/_2048/model/Cell.java
+++ model/src/main/java/de/slash/_2048/model/Cell.java
@@ -20,19 +20,9 @@ public class Cell
         return row;
     }
 
-    public void setRow(int row)
-    {
-        this.row = row;
-    }
-
     public int getColumn()
     {
         return column;
-    }
-
-    public void setColumn(int column)
-    {
-        this.column = column;
     }
 
     public Integer getValue()
